7a8138504b4c7d6562477529388d123e294085f6
JuliaKa74 <146037990+JuliaKa74@users.noreply.github.com>
2023-10-01 16:44:49 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,11 +29,12 @@ streamlit.dataframe(fruits_to_show)
 #API 
 streamlit.header("Fruityvice Fruit Advice!")
 
-try 
+try: 
   fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
   if not fruit_choice:
           streamlit.write('Please select a fruit')
-else fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+else:
+     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
      streamlit.dataframe(fruityvice_normalized)
 
